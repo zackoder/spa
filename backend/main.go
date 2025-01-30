de6ffb3556c2d36ee068286b2dbbcac35ef08c25
zackoder <besszakaria.com>
2025-01-30 20:16:14 +0100

    some changes

--- backend/main.go
+++ backend/main.go
@@ -9,15 +9,15 @@ import (
 func main() {
 	port := ":8080"
 
-	fs := http.FileServer(http.Dir("../frontend"))
+	fs := http.FileServer(http.Dir("./frontend"))
 	http.Handle("/frontend/", http.StripPrefix("/frontend/", fs))
 
-	http.HandleFunc("/", loging)
+	http.HandleFunc("/singin", singin)
 	http.ListenAndServe(port, nil)
 }
 
-func loging(w http.ResponseWriter, r *http.Request) {
-	tmp, err := template.ParseFiles("../index.html")
+func singin(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("./index.html")
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,22 +1,24 @@
 const navbar = document.querySelector(".nav");
 const navLinks = document.querySelectorAll("[data-link]");
 
-console.log(navLinks);
+// console.log(navLinks);
 
-navbar.addEventListener("click", (e) => {
-  let target = e.target;
+// navbar.addEventListener("click", (e) => {
+//   let target = e.target;
 
-  if (target.matches("[data-link]")) {
-    target.preventDefault(); /* preventDefault */
-    console.log(target);
-  }
-});
+//   if (target.matches("[data-link]")) {
+//     target.preventDefault(); /* preventDefault */
+//     console.log(target);
+//   }
+// });
 
 const router = async () => {
   const routes = [
-    { path: "/", view: "/" },
-    { path: "/singin", view: "/singin" },
-    { path: "/singup", view: "/singup" },
+    { path: "/NotFound", isMatch: false },
+    { path: "/", isMatch: true },
+    { path: "/singin", isMatch: false },
+    { path: "/singup", isMatch: false },
+    { path: "/profile", isMatch: false },
   ];
   const potentialmatches = routes.map((route) => {
     return {
@@ -24,4 +26,36 @@ const router = async () => {
       isMatch: location.pathname === route.path,
     };
   });
+  let match = potentialmatches.find(
+    (potentialmatche) => potentialmatche.isMatch
+  );
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    };
+  }
+  console.log(match);
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  router();
+});
+
+function createHTMLel(
+  name,
+  atrebute = { key: "", value: "" },
+  Class = "",
+  content = ""
+) {
+  let element = document.createElement(name);
+  if (content) element.textContent = content;
+  if (Class) element.classlist = Class;
+  if (atrebute.key) element.setAttribute(atrebute.key, atrebute.value);
+  return element;
+}
+
+let test = createHTMLel("a", { key: "data-link", value: "" }, "", "Profile");
+test.href = "/profile";
+let br = createHTMLel("br");
+navbar.append(br, test);
