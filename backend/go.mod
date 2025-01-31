50643fd96c269c7cce17e05d29d893494fa22006
zbessado <bessadouz@gmail.com>
2025-01-31 18:37:22 +0100

    home, signin and signup roots handled

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module reat-time-forum
+
+go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.24 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,102 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	port := ":8080"
+	db, err := sql.Open("sqlite3", "my_db.db")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	fs := http.FileServer(http.Dir("./frontend"))
+	insertdb(db)
+	port := ":8080"
+	fs := http.FileServer(http.Dir("../frontend"))
 	http.Handle("/frontend/", http.StripPrefix("/frontend/", fs))
 
-	http.HandleFunc("/singin", singin)
-	http.ListenAndServe(port, nil)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/signin", signin)
+	http.HandleFunc("/signup", signup)
+
+	if err := (http.ListenAndServe(port, nil))
 }
 
-func singin(w http.ResponseWriter, r *http.Request) {
-	tmp, err := template.ParseFiles("./index.html")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(`{message: Page Not Found}`)
+		return
+	}
+
+	ParsAndExcut(w)
+}
+
+func ParsAndExcut(w http.ResponseWriter) {
+	tmp, err := template.ParseFiles("../index.html")
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	tmp.Execute(w, nil)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	cookie := CheckCookie(r)
+	if cookie != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		fmt.Println("singup")
+		return
+	}
+	ParsAndExcut(w)
+}
+
+func signin(w http.ResponseWriter, r *http.Request) {
+	cookie := CheckCookie(r)
+	if cookie != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		fmt.Println("singin")
+		return
+	}
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		fmt.Println(email, password)
+	}
+	if r.Method == http.MethodGet {
+		ParsAndExcut(w)
+	}
+}
+
+func CheckCookie(r *http.Request) *http.Cookie {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		return nil
+	}
+	return cookie
+}
+
+func insertdb(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		nickname VARCHAR(50),
+		first_name VARCHAR(50),
+		last_name VARCHAR(50),
+		age VARCHAR(50),
+		gender VARCHAR(20),
+		Email VARCHAR(100),
+		password VARCHAR(100)
+	);`
+	_, err := db.Exec(query)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
